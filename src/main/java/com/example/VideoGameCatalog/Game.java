2f822682457124f6d4490a98b18080acc44d0b8f
Adam Seitz <adamseitz98@gmail.com>
2023-06-21 16:07:01 -0400

    Initial files

---
+++ src/main/java/com/example/VideoGameCatalog/Game.java
@@ -0,0 +1,6 @@
+package com.example.VideoGameCatalog;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+public class Game {
+}
---
+++ src/main/java/com/example/VideoGameCatalog/VideoGameCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.example.VideoGameCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoGameCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoGameCatalogApplication.class, args);
+	}
+
+}
