cb06e0bf366ce2b9c99cc7c4333afae2cd328df0
Adam Seitz <adamseitz98@gmail.com>
2023-06-22 21:18:35 -0400

    more ignored files

--- src/main/java/com/example/VideoGameCatalog/Model/Game.java
+++ src/main/java/com/example/VideoGameCatalog/Model/Game.java
@@ -2,5 +2,12 @@ package com.example.VideoGameCatalog;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "games")
 public class Game {
+    @Id
+    private String id;
+    private String title;
+    private String genre;
+    private String developer;
+
 }
